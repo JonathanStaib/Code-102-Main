93cb191b880b5433784ea2b7e35b0fef357e6efd
JonathanStaib <JonathanStaibb@gmail.com>
2022-09-04 17:32:31 -0400

    Updating some JS and HTMl for lab 6 and 7

--- script.js
+++ script.js
@@ -8,27 +8,49 @@ console.log(x);
 const myName = 'Jonathan';
 console.log(myName);
 
-let message;
-let usersName = prompt('What is your name');
-let message; 
-console.log(usersName);
-message = 'Hi, ' + usersName + ' ! ' + ' Welcome to my Page . ' ;
-if (usersName == 'Jonathan'){
+// function functionname(parametes){code to execute}
+
+function greetUser(){
+ let usersName = prompt('What is your name');
+ document.write(' Hello ' + usersName);
+  return usersName;
+}
+
+// greetUser();
+
+function customMessage(customName);{
+ let message; 
+ // console.log(usersName);
+ // message = 'Hi, ' + usersName + ' ! ' + ' Welcome to my Page . ' ;
+
+
+  if (customName == 'Jonathan'){
     message = 'Welcome Shisho!';
-}
-else if(usersName == 'Kassie'){
-message = 'Hello Teach';
-}
-else if(usersName == 'Michael'){
+ }
+  else if(customName == 'Kassie'){
+    message = 'Hello Teach';
+ }
+  else if(CustomName == 'Michael'){
     message = "Wassup Brotha";
-}
-else if(usersName == 'Jill'){
-    message = "Welcome Baby I love you";
-}    
+ }
+  else if(CustomName == 'Jill'){
+    message = "Welcome My Love";
+ }    
 
-else { message = 'Welcome to my site!';
+  else { message = 'Welcome to my site!';
+ }
+
+ document.write(message);
 }
 
+customMessage(customName);
+
+// function addTwoNumbers(num1,num2){
+//     return num1 + num2
+// }
+
+// let myNewNumber = addTwoNumbers(10,10);
+// console.log(myNewNumber)
 
  
 
